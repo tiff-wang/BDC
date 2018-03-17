bf5b63065aae66926a4a31a156f6251923e416e4
John Wu <john.wu@mail.mcgill.ca>
2018-03-17 13:30:36 -0400

    Added muscle pain

--- api/controller/userController.js
+++ api/controller/userController.js
@@ -1,11 +1,40 @@
 var async = require('async')
 var PatientService = require('../service/patientService')
+var Penotypes = require('../mapping/phenotypes')
 
 var user = {}
 
-// function determinePainsArea(patient, callback) {
-//     if (result.phenotypes)
-// }
+function determineDirectPainMapping(patient, callback) {
+    var musclePain = false
+    var jointPain = false
+    async.each(patient.phenotypes, function (phenotype, callback) {
+        if (Penotypes.MUSCLE_PAIN.includes(phenotype)) {
+            musclePain = true
+        }
+        if (Penotypes.JOINT_PAIN.includes(phenotype)) {
+            jointPain = true
+        }
+        return callback()
+    }, function (err) {
+        if (musclePain) {
+            return callback('muscle_pain')
+        } else if (jointPain) {
+            return callback('joint_pain')
+        } else {
+            return callback(null)
+        }
+    })
+}
+
+function determinePainsArea(patient, callback) {
+    determineDirectPainMapping(patient, function(directPain) {
+        if (directPain) {
+            return callback(directPain)
+        } else {
+            return callback('Need to use ML Model')
+        }
+    })
+}
 
 module.exports = {
 
@@ -19,10 +48,14 @@ module.exports = {
                 user.id = result._id
                 async.series([
                     function (callback) {
-                        console.log('Gucci')
+                        determinePainsArea(result, function(area) {
+                            user.pain_area = area
+                        })
+                        return callback()
                     },
                     function (callback){
                         console.log('Gucci')
+                        return callback()
                     }
                 ], function () {
                     return callback(user)
--- api/mapping/phenotypes.js
+++ api/mapping/phenotypes.js
@@ -1,4 +1,9 @@
 const MUSCLE_PAIN = ['5aa552a8f0be1426ee4a75a5', '5aa552a8f0be1426ee4a75a1', '5aa552a8f0be1426ee4a79e8',
     '5aa552a8f0be1426ee4a79e5']
 
-const JOIN_PAIN = ['5aa552a8f0be1426ee4a77b8', '5aa552a8f0be1426ee4a77b6']
+const JOINT_PAIN = ['5aa552a8f0be1426ee4a77b8', '5aa552a8f0be1426ee4a77b6']
+
+module.exports = {
+    MUSCLE_PAIN: MUSCLE_PAIN,
+    JOINT_PAIN: JOINT_PAIN
+}
