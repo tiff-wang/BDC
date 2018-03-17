4be82a165325fd7515bc43d80c31cdd5e848b9e3
tiff-wang <wangtiffany@gmail.com>
2018-03-17 12:06:25 -0400

    added routers

--- public/js/angular/core.js
+++ public/js/angular/core.js
@@ -1,1 +1,8 @@
-angular.module('BDC-app', ['loginController', 'dashboardController']);
+angular.module('BDC-app', ['loginController', 'dashboardController', 'ngRoute'])
+    .config(function($routeProvider) {
+        $routeProvider
+            .when("/login", {
+                templateUrl : "login.html"
+            })
+            .otherwise({redirectTo:'/'})
+    });
