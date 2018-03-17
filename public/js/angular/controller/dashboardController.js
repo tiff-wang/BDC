c8b193f5ecaa623899f0405326d53d02cb6ff5a7
tiff-wang <wangtiffany@gmail.com>
2018-03-17 14:50:10 -0400

    more bindings

--- public/js/angular/controller/dashboardController.js
+++ public/js/angular/controller/dashboardController.js
@@ -5,6 +5,9 @@ angular.module('dashboardController', [])
         $scope.first_name = $scope.user_data.first_name
         $scope.last_name =  $scope.user_data.last_name
         $scope.id =  $scope.user_data.id
+        $scope.pain_area = $scope.user_data.pain_area
+        $scope.strength_area = $scope.user_data.strength_area
+        $scope.workout = $scope.user_data.workout
 
-
+        console.log( $scope.workout)
     }])
